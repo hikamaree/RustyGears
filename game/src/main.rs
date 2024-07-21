769a7989d5519b414c5195e16858a7d0d60232ab
Marko Radosavljevic <marko.r463@gmail.com>
2024-07-21 22:02:09 +0200

    render 3d models

--- game/src/main.rs
+++ game/src/main.rs
@@ -2,17 +2,12 @@
 #![allow(non_snake_case)]
 
 extern crate gl;
-use self::gl::types::*;
-
-use std::ptr;
-use std::mem;
-use std::os::raw::c_void;
 use std::ffi::CStr;
 
 use rusty_gears::*;
 
 
-use cgmath::{Matrix4, Vector3, vec3,  Deg, perspective, Point3};
+use cgmath::{Matrix4, vec3,  Deg, perspective, Point3};
 
 
 const SCR_WIDTH: u32 = 1280;
@@ -29,82 +24,9 @@ pub fn main() {
 
     let mut window = Window::new(SCR_WIDTH, SCR_HEIGHT, "RustyGears");
 
-    let (ourShader, _VBO, VAO, texture, cubePositions) = unsafe {
+    let shader = Shader::new("shaders/vertex_shader.vs", "shaders/fragment_shader.fs");
 
-        let shader = Shader::new("shaders/vertex_shader.vs", "shaders/fragment_shader.fs");
-
-        let vertices: [f32; 180] = [
-             -0.5, -0.5, -0.5,  0.0, 0.0,
-              0.5, -0.5, -0.5,  1.0, 0.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-             -0.5,  0.5, -0.5,  0.0, 1.0,
-             -0.5, -0.5, -0.5,  0.0, 0.0,
-
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-              0.5, -0.5,  0.5,  1.0, 0.0,
-              0.5,  0.5,  0.5,  1.0, 1.0,
-              0.5,  0.5,  0.5,  1.0, 1.0,
-             -0.5,  0.5,  0.5,  0.0, 1.0,
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-
-             -0.5,  0.5,  0.5,  1.0, 0.0,
-             -0.5,  0.5, -0.5,  1.0, 1.0,
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-             -0.5,  0.5,  0.5,  1.0, 0.0,
-
-              0.5,  0.5,  0.5,  1.0, 0.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-              0.5, -0.5, -0.5,  0.0, 1.0,
-              0.5, -0.5, -0.5,  0.0, 1.0,
-              0.5, -0.5,  0.5,  0.0, 0.0,
-              0.5,  0.5,  0.5,  1.0, 0.0,
-
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-              0.5, -0.5, -0.5,  1.0, 1.0,
-              0.5, -0.5,  0.5,  1.0, 0.0,
-              0.5, -0.5,  0.5,  1.0, 0.0,
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-
-             -0.5,  0.5, -0.5,  0.0, 1.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-              0.5,  0.5,  0.5,  1.0, 0.0,
-              0.5,  0.5,  0.5,  1.0, 0.0,
-             -0.5,  0.5,  0.5,  0.0, 0.0,
-             -0.5,  0.5, -0.5,  0.0, 1.0
-        ];
-
-        let cubePositions: [Vector3<f32>; 4] = [vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), vec3(1.0, 0.0, 0.0), vec3(-1.0, 0.0, 0.0)];
-        let (mut VBO, mut VAO) = (0, 0);
-        gl::GenVertexArrays(1, &mut VAO);
-        gl::GenBuffers(1, &mut VBO);
-
-        gl::BindVertexArray(VAO);
-
-        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
-        gl::BufferData(gl::ARRAY_BUFFER,
-                       (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                       &vertices[0] as *const f32 as *const c_void,
-                       gl::STATIC_DRAW);
-
-        let stride = 5 * mem::size_of::<GLfloat>() as GLsizei;
-
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
-        gl::EnableVertexAttribArray(0);
-        gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, stride, (3 * mem::size_of::<GLfloat>()) as *const c_void);
-        gl::EnableVertexAttribArray(1);
-
-
-        let texture = loadTexture("resources/textures/block.jpg");
-
-        shader.useProgram();
-        shader.setInt(c_str!("texture1"), 0);
-
-        (shader, VBO, VAO, texture, cubePositions)
-    };
+    let cube = Model::new("resources/models/block/block.obj");
 
     while !window.should_close() {
         window.process_events(&mut lastX, &mut lastY, &mut camera);
@@ -114,24 +36,15 @@ pub fn main() {
         Window::clear(0.2, 0.2, 0.2, 1.0);
 
         unsafe {
-
-            gl::ActiveTexture(gl::TEXTURE0);
-            gl::BindTexture(gl::TEXTURE_2D, texture);
-
-            ourShader.useProgram();
+            shader.useProgram();
 
             let projection: Matrix4<f32> = perspective(Deg(camera.Zoom), SCR_WIDTH as f32 / SCR_HEIGHT as f32 , 0.1, 100.0);
-            ourShader.setMat4(c_str!("projection"), &projection);
+            shader.setMat4(c_str!("projection"), &projection);
 
             let view = camera.GetViewMatrix();
-            ourShader.setMat4(c_str!("view"), &view);
+            shader.setMat4(c_str!("view"), &view);
 
-            gl::BindVertexArray(VAO);
-            for (_, position) in cubePositions.iter().enumerate() {
-                let model: Matrix4<f32> = Matrix4::from_translation(*position);
-                ourShader.setMat4(c_str!("model"), &model);
-                gl::DrawArrays(gl::TRIANGLES, 0, 36);
-            }
+            cube.Draw(&shader, vec3(0.0, 0.0, 0.0));
         }
 
         window.update();
