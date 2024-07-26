a8ab90f0c43ac795ebe901ebd8635161d6ec0d88
Marko Radosavljevic <marko.r463@gmail.com>
2024-07-26 14:46:20 +0200

    shadows

--- game/src/main.rs
+++ game/src/main.rs
@@ -4,28 +4,51 @@ pub fn main() {
     let mut window = Window::new(1280, 720, "RustyGears");
     let mut camera = Camera::default();
 
-    let shader = Shader::new("shaders/vertex_shader.vs", "shaders/fragment_shader.fs");
-    let depth_shader = Shader::new("shaders/depth_shader.vs", "shaders/depth_shader.fs");
+    let shader = Shader::new("shaders/vertex_shader.glsl", "shaders/fragment_shader.glsl");
+    let depth_shader = Shader::new("shaders/depth_vertex_shader.glsl", "shaders/depth_fragment_shader.glsl");
 
-    let ambient_light = AmbientLight::new(vec3(0.2, 0.2, 0.2), 2.5);
-    let light_source1 = LightSource::new(vec3(10.0, 10.0, 0.0), vec3(1.0, 1.0, 1.0), 10.0);
-    let light_source2 = LightSource::new(vec3(-2.0, -2.0, -2.0), vec3(1.0, 1.0, 1.0), 1.0);
+    let ambient_light = AmbientLight::new(vec3(0.2, 0.2, 0.2), 2.0);
+    let light_source1 = LightSource::new(vec3(-5.0, 10.0, -10.0), vec3(1.0, 1.0, 1.0), 1.0);
+    //let light_source2 = LightSource::new(vec3(5.0, 10.0, 10.0), vec3(1.0, 1.0, 1.0), 0.0);
 
-    let big_block = Model::new("resources/models/big_block/big_block.obj", vec3(0.0, 0.0, 0.0));
-    let ball = Model::new("resources/models/block/block.obj", vec3(0.0, 0.0, 0.0));
+    let plane = Model::new("resources/models/plane/plane.obj", vec3(0.0, -0.56, 0.0));
+    let car = Model::new("resources/models/car/Avent_sport.obj", vec3(0.0, 0.0, 0.0));
+    let ball = Model::new("resources/models/ball/ball.obj", vec3(1.5, 3.0, 0.0));
+    let block = Model::new("resources/models/block/block.obj", vec3(0.0, 7.0, 0.0));
 
-    let fog = Fog::new(vec3(0.2, 0.2, 0.2), 0.2);
+    let fog = Fog::new(vec3(0.2, 0.2, 0.2), 0.05);
 
     let mut scene = Scene::new();
-    scene.add_model(big_block);
     scene.add_model(ball);
+    scene.add_model(block);
+    scene.add_model(car);
+    scene.add_model(plane);
+
     scene.add_light_source(light_source1);
-    scene.add_light_source(light_source2);
+    //scene.add_light_source(light_source2);
     scene.set_ambient_light(ambient_light);
     scene.set_fog(fog);
 
+
+    use std::time::{Duration, Instant};
+
+    let mut last_time = Instant::now();
+    let mut frame_count = 0;
+
     while !window.should_close() {
-        Window::clear(1.0, 1.0, 1.0, 1.0);
+
+        frame_count += 1;
+
+        let elapsed = last_time.elapsed();
+        if elapsed >= Duration::new(1, 0) {
+            let fps = frame_count;
+            frame_count = 0;
+            last_time = Instant::now();
+            println!("FPS: {}", fps);
+        }
+
+
+        Window::clear(0.7, 0.8, 0.9, 1.0);
 
         if window.key_pressed('W') {
             camera.move_forward(window.delta_time);
--- src/graphics/model.rs
+++ src/graphics/model.rs
@@ -1,19 +1,15 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
-use std::os::raw::c_void;
 use std::path::Path;
 use std::ffi::CStr;
 
 use cgmath::{vec2, vec3, vec4, Vector3, Matrix4};
-use gl;
-use image;
-use image::DynamicImage::*;
-use image::GenericImage;
 use tobj;
 
 use super::mesh::{ Mesh, Texture, Vertex };
 use super::shader::Shader;
+use super::utils::*;
 
 pub struct Model {
     pub meshes: Vec<Mesh>,
@@ -98,7 +94,11 @@ impl Model {
                     textures.push(texture);
                 } else {
                     for vertex in vertices.iter_mut() {
-                        vertex.Color = vec4(material.diffuse[0], material.diffuse[1], material.diffuse[2], 1.0);
+                        if material.diffuse.len() > 3 {
+                            vertex.Color = vec4(material.diffuse[0], material.diffuse[1], material.diffuse[2], material.diffuse[3]);
+                        } else {
+                            vertex.Color = vec4(material.diffuse[0], material.diffuse[1], material.diffuse[2], 1.0);
+                        }
                     }
                 }
                 if !material.specular_texture.is_empty() {
@@ -125,41 +125,11 @@ impl Model {
         }
 
         let texture = Texture {
-            id: unsafe { TextureFromFile(path, &self.directory) },
+            id: loadTexture(format!("{}/{}", self.directory, path).as_str()),
             type_: typeName.into(),
             path: path.into()
         };
         self.textures_loaded.push(texture.clone());
         texture
     }
-}
-
-unsafe fn TextureFromFile(path: &str, directory: &str) -> u32 {
-    let filename = format!("{}/{}", directory, path);
-
-    let mut textureID = 0;
-    gl::GenTextures(1, &mut textureID);
-
-    let img = image::open(&Path::new(&filename)).expect("Texture failed to load");
-    let img = img.flipv();
-    let format = match img {
-        ImageLuma8(_) => gl::RED,
-        ImageLumaA8(_) => gl::RG,
-        ImageRgb8(_) => gl::RGB,
-        ImageRgba8(_) => gl::RGBA,
-    };
-
-    let data = img.raw_pixels();
-
-    gl::BindTexture(gl::TEXTURE_2D, textureID);
-    gl::TexImage2D(gl::TEXTURE_2D, 0, format as i32, img.width() as i32, img.height() as i32,
-    0, format, gl::UNSIGNED_BYTE, &data[0] as *const u8 as *const c_void);
-    gl::GenerateMipmap(gl::TEXTURE_2D);
-
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR as i32);
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
-
-    textureID
 }
